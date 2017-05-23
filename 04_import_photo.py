258ff07e5be2207c2ca6382fdff96708771c6bf9
yuntux <yuntux@gmail.com>
2017-05-23 13:59:10 +0200

    Create 04_import_photo.py

---
+++ 04_import_photo.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import datetime
+import time
+import erppeek
+import os
+from os import listdir
+import base64
+
+from cfg_secret_configuration import odoo_configuration_user
+
+
+###############################################################################
+# Odoo Connection
+###############################################################################
+def init_openerp(url, login, password, database):
+    openerp = erppeek.Client(url)
+    uid = openerp.login(login, password=password, database=database)
+    user = openerp.ResUsers.browse(uid)
+    tz = user.tz
+    return openerp, uid, tz
+
+openerp, uid, tz = init_openerp(
+    odoo_configuration_user['url'],
+    odoo_configuration_user['login'],
+    odoo_configuration_user['password'],
+    odoo_configuration_user['database'])
+
+
+###############################################################################
+# Configuration
+###############################################################################
+
+# Quantity of moves to batch at the same time
+QTY_TO_BATCH = 10
+
+# Pause duration between two batches
+SLEEP_TIME = 0.0
+
+###############################################################################
+# Script
+###############################################################################
+
+def send_image(liste, folder_path):
+    print "number of objects in folder path : ", len(listdir(folder_path))
+    count = 0
+    for m in liste :
+            file_path = folder_path+"/"+str(m.ref)+".JPG"
+            if os.path.isfile(file_path):
+                with open(file_path, "rb") as image_file:
+                    encoded_string = base64.b64encode(image_file.read())
+                    print "send picture"
+                    m.image = encoded_string
+                    print '   => photo enregistrée'
+            else:
+                print "    => pas de photo"
+            count=count+1
+            print count
+
+
+associated_people_ids = openerp.search('res.partner', [('is_associated_people', '=', True), ('barcode_base', '!=', False)])
+members_ids = openerp.search('res.partner', [('is_associated_people', '=', False), ('barcode_base', '!=', False)])
+
+associated_poeple = openerp.ResPartner.browse([('id', 'in', associated_people_ids)])
+members = openerp.ResPartner.browse([('id', 'in', members_ids)])
+
+print ">>>>>>>> Number of members people found: ", len(members)
+send_image(members, "/Users/adumaine/Downloads/valides/cooperateurs/")
+
+print ">>>>>>>> Number of associated people found: ", len(associated_people)
+send_image(associated_people, "~/Downloads/valides/rattaches")
