dc42b0bc65e533ba3b19e7a4efee6c103f93e310
yuntux <yuntux@gmail.com>
2017-05-23 17:26:51 +0200

    Update 04_import_photo.py

--- 04_import_photo.py
+++ 04_import_photo.py
@@ -27,12 +27,14 @@ openerp, uid, tz = init_openerp(
     odoo_configuration_user['password'],
     odoo_configuration_user['database'])
 
-
 ###############################################################################
 # Script
 ###############################################################################
 
+
 def send_image(liste, folder_path,bool_assoc):
+    no_picture = 0
+    picture = 0
     print "number of objects in folder path : ", len(listdir(folder_path))
     count = 0
     for m in liste :
@@ -42,24 +44,28 @@ def send_image(liste, folder_path,bool_assoc):
             print file_path, m.name
             if os.path.isfile(file_path):
                 with open(file_path, "rb") as image_file:
-                    encoded_string = base64.b64encode(image_file.read())
-                    print "send picture"
-                    m.image = encoded_string
+                    content = image_file.read()
+                    encoded_string = base64.b64encode(content)
+                    m.write({'image' : encoded_string})
                     print '   => photo enregistrée'
+                    picture=picture+1
             else:
+                no_picture = no_picture+1
                 print "    => pas de photo"
             count=count+1
             print count
 
+    print "avec photo", picture
+    print "sans photo", no_picture
 
 associated_people_ids = openerp.search('res.partner', [('is_associated_people', '=', True), ('barcode_base', '!=', False)])
 members_ids = openerp.search('res.partner', [('is_associated_people', '=', False), ('barcode_base', '!=', False)])
 
-associated_poeple = openerp.ResPartner.browse([('id', 'in', associated_people_ids)])
+associated_people = openerp.ResPartner.browse([('id', 'in', associated_people_ids)])
 members = openerp.ResPartner.browse([('id', 'in', members_ids)])
 
 print ">>>>>>>> Number of members people found: ", len(members)
-send_image(members, "/home/louve-erp-dev/photos/cooperateurs/", False)
+send_image(members, "/home/louve-erp-prod/photos/cooperateurs/", False)
 
 print ">>>>>>>> Number of associated people found: ", len(associated_people)
-send_image(associated_people, "/home/louve-erp-dev/photos/rattaches", True)
+send_image(associated_people, "/home/louve-erp-prod/photos/rattaches/", True)
